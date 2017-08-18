46ebcef2b83b3b56c0287562b6c9ae4b31053aa1
Christopher Chen <christopher.chen1124@gmail.com>
2017-08-17 20:28:04 -0700

    Committing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,18 +2,19 @@ var wins=0;
 var losses=0;
 var guesses_left=9;
 var guesses_so_far=[];
-var computerChoices=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var computerChoices=['a','b','c','d','e','f','g','h','i','j'];
+var computerGuess=computerChoices[Math.floor(Math.random()*computerChoices.length)];
 
 document.onkeyup=function(event)
 {
 	var userGuess=event.key;
-	var computerGuess=computerChoices[Math.floor(Math.random()*computerChoices.length)];
 	var html;
-
-	if(userGuess==computerGuess)
+	if(userGuess===computerGuess)
 	{
 		wins++;
 		guesses_left=9;
+		computerGuess=computerChoices[Math.floor(Math.random()*computerChoices.length)];
+
 	}
 	else
 	{
@@ -24,8 +25,8 @@ document.onkeyup=function(event)
 	{
 		losses++;
 		guesses_left=9;
+		computerGuess=computerChoices[Math.floor(Math.random()*computerChoices.length)];
 	}
-	
 	myguesses(guesses_so_far,userGuess);
 	html="<p> Wins: "+wins+"</p><br>"+"<p> Losses: "+losses+"</p><br>"+
 	"<p>Guesses Left: "+guesses_left+"</p><br>"+"<p>Your guesses so far: "+
